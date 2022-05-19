2ecb5539d9acce9e6e56a937dd31a5d8ddc067de
AmirAbasi <amir.abasi.dev@gmail.com>
2022-05-19 09:31:57 +0430

    feat : 🏗 config pages to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Suspense } from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+
 import Header from "./components/UI/header/Header";
+import Spinner from "./components/UI/spinner/Spinner";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
-  )
-}
+      <div className="container">
+        <Suspense fallback={<Spinner minHeight="70vh"/>}>
+          <Routes>
+            {/* Pages */}
+          </Routes>
+        </Suspense>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
