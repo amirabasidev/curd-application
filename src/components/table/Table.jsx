6fbaccb8fbe55173e21f401b4ae2d271adea069a
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-01 22:16:24 +0430

    feat : ✨ add pagination table

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
+
 import Spinner from "../UI/spinner/Spinner";
 
 import classes from "./table.module.css";
 
 const Table = ({ headData, bodyData, renderBody, loading }) => {
+  const [currentPage, setCurrentPage] = useState(5);
+
+  const countPage = bodyData.length / 5;
+  const bodyDataCurrent = bodyData.slice(currentPage - 5, currentPage);
+
+  const handlePageClick = (data) => {
+    const currentPage = (data.selected + 1) * 5;
+    setCurrentPage(currentPage);
+  };
+
   return (
     <div className={classes.table__wrapper}>
       <table className={classes.table}>
@@ -17,11 +30,28 @@ const Table = ({ headData, bodyData, renderBody, loading }) => {
         )}
         {bodyData && renderBody && !loading && (
           <tbody>
-            {bodyData.map((item, index) => renderBody(item, index))}
+            {bodyDataCurrent.map((item, index) => renderBody(item, index))}
           </tbody>
         )}
       </table>
-        {loading && <Spinner minHeight="180px"/>}
+      {loading && <Spinner minHeight="180px" />}
+      {bodyData.length > 0 && (
+        <ReactPaginate
+          previousLabel="previous"
+          nextLabel="next"
+          breakLabel="..."
+          pageCount={countPage}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={3}
+          onPageChange={handlePageClick}
+          containerClassName={classes.table__pagination}
+          pageLinkClassName={classes.table__pagination_item}
+          previousLinkClassName={classes.table__pagination_item}
+          nextLinkClassName={classes.table__pagination_item}
+          breakLinkClassName={classes.table__pagination_item}
+          activeClassName={classes.table__pagination_item_active}
+        />
+      )}
     </div>
   );
 };
